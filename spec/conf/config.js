49f563e6e136a7ed476fedb4fdca8914ccf50899
Ryan Anderson <anderson.ryanc@gmail.com>
2015-06-17 17:59:27 -0700

    config file

---
+++ spec/conf/config.js
@@ -0,0 +1,17 @@
+require( 'dotenv' ).load();
+var btoa = require( 'btoa' );
+
+var userAgent = 'Node.JS/' + process.versions.node + ' (' +
+  process.env._system_name + ' ' + process.env._system_version +
+  ') Jasmine-Node/1.14.5 Frisby.JS/0.8.5';
+
+var config = {
+  endpoint: 'https://' + ( process.env.API_ENDPOINT || 'dev.events.7apps.io' ) + '/v1.2',
+  version: '1.2',
+  ua: userAgent,
+  user: process.env.API_USER || '',
+  pass: process.env.API_PASS || '',
+};
+config.auth = btoa( config.user + ':' + config.pass );
+
+module.exports = config;
