c6a8a0fd537ad736291000f9587e32fe36b384cb
Ryan Anderson <anderson.ryanc@gmail.com>
2015-12-10 09:26:28 -0800

    sample spec syntax

--- test/sample_spec.js
+++ test/sample_spec.js
@@ -1,49 +1,47 @@
-var request = require('request');
-var config  = require('./conf/config');
-var customMatchers = require('./conf/customMatchers');
+var request        = require( 'request' );
+var config         = require( './conf/config' );
+var customMatchers = require( './conf/customMatchers' );
 
-describe("/verify", function() {
-	var json;
+describe( '/verify', function() {
+  var json;
 
-	beforeEach(function() {
-		jasmine.addMatchers( customMatchers );
-	});
+  beforeEach( function() {
+    jasmine.addMatchers( customMatchers );
+  });
 
-	beforeAll(function(done) {
+  beforeAll( function( done ) {
+    console.log( 'GET ' + config.endpoint + '/verify' );
+    request.get({
+      url: config.endpoint + '/verify',
+      headers: {
+        'x-version': config.version,
+        'User-Agent': config.ua,
+      },
+      auth: {
+        username: config.user,
+        password: config.pass,
+        sendImmediately: true,
+      },
+    }, function( err, res, body ) {
+      json = JSON.parse( body );
+      // console.log( json );
+      done();
+    });
+  });
 
-		console.log('GET ' + config.endpoint + '/verify');
-		request.get({
-			url: config.endpoint + '/verify',
-			headers: {
-				'x-version': config.version,
-				'User-Agent': config.ua
-			},
-			auth: {
-				username: config.user,
-				password: config.pass,
-				sendImmediately: true
-			},
-		}, function(err, res, body) {
-			json = JSON.parse( body );
-			// console.log( json );
-			done();
-		});
+  it( 'contains a user object', function( done ) {
+    expect( json.user ).toBeDefined();
+    done();
+  });
 
-	});
+  it( 'contains a user object with valid attributes', function( done ) {
+    expect( json.user.id ).toBeDefined();
+    expect( json.user.id ).toBeType( String );
 
-	it("contains a user object", function(done) {
-		expect( json.user ).toBeDefined();
-		done();
-	});
-
-	it("contains a user object with valid attributes", function(done) {
-		expect( json.user.id ).toBeDefined();
-		expect( json.user.id ).toBeType( String );
-
-		expect( json.user.username ).toBeDefined();
-		expect( json.user.email ).toBeDefined();
-		expect( json.user.group ).toBeDefined();
-		done();
-	});
+    expect( json.user.username ).toBeDefined();
+    expect( json.user.email ).toBeDefined();
+    expect( json.user.group ).toBeDefined();
+    done();
+  });
 
 });
